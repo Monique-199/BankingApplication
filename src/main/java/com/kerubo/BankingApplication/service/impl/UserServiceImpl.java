f986c31c6b90b5a87df19df26669867e376ca1ee
Monicah Kerubo <106078042+Monique-199@users.noreply.github.com>
2025-01-13 12:58:58 +0300

    Update UserServiceImpl.java

--- src/main/java/com/kerubo/BankingApplication/service/impl/UserServiceImpl.java
+++ src/main/java/com/kerubo/BankingApplication/service/impl/UserServiceImpl.java
@@ -6,7 +6,7 @@ import com.kerubo.BankingApplication.entity.Role;
 import com.kerubo.BankingApplication.entity.User;
 import com.kerubo.BankingApplication.repository.UserRepository;
 import com.kerubo.BankingApplication.util.AccountUtils;
-import lombok.AllArgsConstructor;
+import lombok.Generated;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,13 +16,13 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-@AllArgsConstructor
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
+
     @Autowired
     AuthenticationManager authenticationManager;
 
@@ -31,264 +31,134 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     TransactionService transactionService;
+
     @Autowired
     JwtTokenProvider jwtTokenProvider;
 
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    @Override
+    // Method to create a new account for a user
     public BankResponse createAccount(UserRequest userRequest) {
-        if (userRepository.existsByEmail(userRequest.getEmail())) {
+        // Check if an account already exists with the provided email
+        if (this.userRepository.existsByEmail(userRequest.getEmail())) {
             return BankResponse.builder()
-                    .responseCode(AccountUtils.ACCOUNT_EXISTS_CODE)
-                    .responseMessage(AccountUtils.ACCOUNT_EXISTS_MESSAGE)
-                    .accountInfo(null)
-                    .build();
-        }
-
-        // Creating a new user and populating with data from UserRequest
-        User newUser = User.builder()
-                .firstName(userRequest.getFirstName())
-                .lastName(userRequest.getLastName())
-                .gender(userRequest.getGender())
-                .othername(userRequest.getOthername())
-                .address(userRequest.getAddress())
-                .stateOfOrigin(userRequest.getStateOfOrigin())
-                .accountNumber(AccountUtils.generateAccountNumber())
-                .accountBalance(BigDecimal.ZERO)
-                .password(passwordEncoder.encode(userRequest.getPassword()))
-                .email(userRequest.getEmail())
-                .phoneNumber(userRequest.getPhoneNumber())
-                .alternativePhoneNumber(userRequest.getAlternativePhoneNumber())
-                .status("ACTIVE")
-                .role(Role.valueOf("ROLE_ADMIN"))
-                .build();
-
-        User savedUser = userRepository.save(newUser);
-
-        // Send email alerts
-        EmailDetails emailDetails = EmailDetails.builder()
-                .recipient(savedUser.getEmail())
-                .subject("ACCOUNT CREATION")
-                .messageBody("Congratulations your account has been successfully created!.\n Your account details \n" +
-                        "Account name : " + savedUser.getFirstName() + " " + savedUser.getLastName() + " " + savedUser.getOthername() +
-                        "\nAccount number : " + savedUser.getAccountNumber())
-                .build();
-
-        emailService.sendEmailAlert(emailDetails);
-
-        return BankResponse.builder()
-                .responseCode(AccountUtils.ACCOUNT_CREATION_SUCCESS)
-                .responseMessage(AccountUtils.ACCOUNT_CREATION_MESSAGE)
-                .accountInfo(AccountInfo.builder()
-                        .accountBalance(savedUser.getAccountBalance())
-                        .accountNumber(savedUser.getAccountNumber())
-                        .accountName(savedUser.getFirstName() + " " + savedUser.getLastName() + " " + savedUser.getOthername())
-                        .build())
-                .build();
-    }
-    public BankResponse login(LoginDto loginDto){
-        Authentication authentication = null;
-        authentication= authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginDto.getEmail(),loginDto.getPassword())
-        );
-        EmailDetails loginAlert = EmailDetails.builder()
-                .subject("You're logged in!")
-                .recipient(loginDto.getEmail())
-                .messageBody("You logged in to your account. If you did not initiate this transanction, please contact your bank")
-                .build();
-        emailService.sendEmailAlert(loginAlert);
-        return BankResponse.builder()
-                .responseCode("Login Success")
-                .responseMessage(jwtTokenProvider.generateToken(authentication))
-                .build();
-
-    }
-    @Override
-    public BankResponse balanceInquiry(InquiryRequest request) {
-        // Check if the account exists in the database
-        boolean isAccountExist = userRepository.existsByAccountNumber(request.getAccountNumber());
-        if (!isAccountExist) {
-            return BankResponse.builder()
-                    .responseCode(AccountUtils.ACCOUNT_NOT_EXIST_CODE)
-                    .responseMessage(AccountUtils.ACCOUNT_NOT_EXIST_MESSAGE)
-                    .accountInfo(null)
-                    .build();
-        }
-
-        // Use the instance 'userRepository' to find the user by account number
-        User foundUser = userRepository.findByAccountNumber(request.getAccountNumber());
-        return BankResponse.builder()
-                .responseCode(AccountUtils.ACCOUNT_FOUND_CODE)
-                .responseMessage(AccountUtils.ACCOUNT_FOUND_MESSAGE)
-                .accountInfo(AccountInfo.builder()
-                        .accountBalance(foundUser.getAccountBalance())
-                        .accountNumber(request.getAccountNumber())
-                        .accountName(foundUser.getFirstName() + " " + foundUser.getLastName() + " " + foundUser.getOthername())
-                        .build())
-                .build();
-    }
-
-    @Override
-    public String nameInquiry(InquiryRequest request) {
-        boolean isAccountExist = userRepository.existsByAccountNumber(request.getAccountNumber());
-        if (!isAccountExist) {
-            return AccountUtils.ACCOUNT_NOT_EXIST_MESSAGE;
-        }
-
-        // Fetch the user by account number
-        User foundUser = userRepository.findByAccountNumber(request.getAccountNumber());
-        return foundUser.getFirstName() + " " + foundUser.getLastName() + " " + foundUser.getOthername();
-    }
-
-    @Override
-    public BankResponse creditAccount(CreditDebitRequest request) {
-        // Check if the account exists
-        boolean isAccountExist = userRepository.existsByAccountNumber(request.getAccountNumber());
-        if (!isAccountExist) {
-            return BankResponse.builder()
-                    .responseCode(AccountUtils.ACCOUNT_NOT_EXIST_CODE)
-                    .responseMessage(AccountUtils.ACCOUNT_NOT_EXIST_MESSAGE)
-                    .accountInfo(null)
-                    .build();
-        }
-
-        // Fetch the user by account number
-        User userToCredit = userRepository.findByAccountNumber(request.getAccountNumber());
-        userToCredit.setAccountBalance(userToCredit.getAccountBalance().add(request.getAmount()));
-        userRepository.save(userToCredit);
-
-        // Save the transaction
-        TransactionDto transactionDto = TransactionDto.builder()
-                .accountNumber(userToCredit.getAccountNumber())
-                .transanctionType("CREDIT")
-                .amount(request.getAmount())
-                .build();
-
-        transactionService.saveTransaction(transactionDto);
-
-        return BankResponse.builder()
-                .responseCode(AccountUtils.ACCOUNT_CREDITED_SUCCESS)
-                .responseMessage(AccountUtils.ACCOUNT_CREDITED_SUCCESS_MESSAGE)
-                .accountInfo(AccountInfo.builder()
-                        .accountName(userToCredit.getFirstName() + " " + userToCredit.getLastName() + " " + userToCredit.getOthername())
-                        .accountBalance(userToCredit.getAccountBalance())
-                        .accountNumber(request.getAccountNumber())
-                        .build())
-                .build();
-    }
-
-    @Override
-    public BankResponse debitAccount(CreditDebitRequest request) {
-        // Check if the account exists
-        boolean isAccountExist = userRepository.existsByAccountNumber(request.getAccountNumber());
-        if (!isAccountExist) {
-            return BankResponse.builder()
-                    .responseCode(AccountUtils.ACCOUNT_NOT_EXIST_CODE)
-                    .responseMessage(AccountUtils.ACCOUNT_NOT_EXIST_MESSAGE)
-                    .accountInfo(null)
-                    .build();
-        }
-
-        // Fetch the user by account number
-        User userToDebit = userRepository.findByAccountNumber(request.getAccountNumber());
-        BigInteger availableBalance = userToDebit.getAccountBalance().toBigInteger();
-        BigInteger debitAmount = request.getAmount().toBigInteger();
-        if (availableBalance.intValue() < debitAmount.intValue()) {
-            return BankResponse.builder()
-                    .responseCode(AccountUtils.INSUFFICIENT_ACCOUNT_BALANCE_CODE)
-                    .responseMessage(AccountUtils.INSUFFICIENT_BALANCE_MESSAGE)
+                    .responseCode("001")
+                    .responseMessage("A user already has an account created")
                     .accountInfo(null)
                     .build();
         } else {
-            userToDebit.setAccountBalance(userToDebit.getAccountBalance().subtract(request.getAmount()));
-            userRepository.save(userToDebit);
-
-            // Save the transaction
-            TransactionDto transactionDto = TransactionDto.builder()
-                    .accountNumber(userToDebit.getAccountNumber())
-                    .transanctionType("DEBIT")
-                    .amount(request.getAmount())
+            // Build a new User object with provided details
+            User newUser = User.builder()
+                    .firstName(userRequest.getFirstName())
+                    .lastName(userRequest.getLastName())
+                    .gender(userRequest.getGender())
+                    .othername(userRequest.getOthername())
+                    .address(userRequest.getAddress())
+                    .stateOfOrigin(userRequest.getStateOfOrigin())
+                    .accountNumber(AccountUtils.generateAccountNumber())
+                    .accountBalance(BigDecimal.ZERO)
+                    .password(this.passwordEncoder.encode(userRequest.getPassword()))
+                    .email(userRequest.getEmail())
+                    .phoneNumber(userRequest.getPhoneNumber())
+                    .alternativePhoneNumber(userRequest.getAlternativePhoneNumber())
+                    .status("ACTIVE")
+                    .role(Role.valueOf("ROLE_ADMIN"))
                     .build();
 
-            transactionService.saveTransaction(transactionDto);
+            // Save the user to the database
+            User savedUser = this.userRepository.save(newUser);
 
+            // Build email details for account creation notification
+            EmailDetails emailDetails = EmailDetails.builder()
+                    .recipient(savedUser.getEmail())
+                    .subject("ACCOUNT CREATION")
+                    .messageBody("Congratulations your account has been successfully created!\n" +
+                            "Your account details:\n" +
+                            "Account name: " + savedUser.getFirstName() + " " + savedUser.getLastName() + " " + savedUser.getOthername() + "\n" +
+                            "Account number: " + savedUser.getAccountNumber())
+                    .build();
+
+            // Send email notification
+            this.emailService.sendEmailAlert(emailDetails);
+
+            // Return success response with account details
             return BankResponse.builder()
-                    .responseCode(AccountUtils.ACCOUNT_DEBITED_SUCCESS_CODE)
-                    .responseMessage(AccountUtils.ACCOUNT_DEBITED_SUCCESS_MESSAGE)
+                    .responseCode("002")
+                    .responseMessage("Account has been successfully created!")
                     .accountInfo(AccountInfo.builder()
-                            .accountNumber(request.getAccountNumber())
-                            .accountName(userToDebit.getFirstName() + " " + userToDebit.getLastName() + " " + userToDebit.getOthername())
-                            .accountBalance(userToDebit.getAccountBalance())
+                            .accountBalance(savedUser.getAccountBalance())
+                            .accountNumber(savedUser.getAccountNumber())
+                            .accountName(savedUser.getFirstName() + " " + savedUser.getLastName() + " " + savedUser.getOthername())
                             .build())
                     .build();
         }
     }
 
-    @Override
-    public BankResponse transfer(TransferRequest request) {
-        // Check if the destination account exists
-        boolean isDestinationAccountExist = userRepository.existsByAccountNumber(request.getDestinationAccountNumber());
-        if (!isDestinationAccountExist) {
-            return BankResponse.builder()
-                    .responseCode(AccountUtils.ACCOUNT_NOT_EXIST_CODE)
-                    .responseMessage(AccountUtils.ACCOUNT_NOT_EXIST_MESSAGE)
-                    .accountInfo(null)
-                    .build();
-        }
+    // Method for user login
+    public BankResponse login(LoginDto loginDto) {
+        // Authenticate user credentials
+        Authentication authentication = this.authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword()));
 
-        // Fetch the source and destination accounts
-        User sourceAccountUser = userRepository.findByAccountNumber(request.getSourceAccountNumber());
-        User destinationAccountUser = userRepository.findByAccountNumber(request.getDestinationAccountNumber());
-
-        if (request.getAmount().compareTo(sourceAccountUser.getAccountBalance()) > 0) {
-            return BankResponse.builder()
-                    .responseCode(AccountUtils.INSUFFICIENT_ACCOUNT_BALANCE_CODE)
-                    .responseMessage(AccountUtils.INSUFFICIENT_BALANCE_MESSAGE)
-                    .accountInfo(null)
-                    .build();
-        }
-
-        // Debit the source account
-        sourceAccountUser.setAccountBalance(sourceAccountUser.getAccountBalance().subtract(request.getAmount()));
-        userRepository.save(sourceAccountUser);
-
-        // Send debit alert
-        String sourceUserName = sourceAccountUser.getFirstName() + " " + sourceAccountUser.getLastName() + " " + sourceAccountUser.getOthername();
-        EmailDetails debitAlert = EmailDetails.builder()
-                .subject("Debit Alert")
-                .recipient(sourceAccountUser.getEmail())
-                .messageBody("The sum of " + request.getAmount() + " has been deducted from your account! Your current balance is: " + sourceAccountUser.getAccountBalance())
+        // Send email notification for successful login
+        EmailDetails loginAlert = EmailDetails.builder()
+                .subject("You're logged in!")
+                .recipient(loginDto.getEmail())
+                .messageBody("You logged in to your account. If you did not initiate this transaction, please contact your bank.")
                 .build();
-        emailService.sendEmailAlert(debitAlert);
+        this.emailService.sendEmailAlert(loginAlert);
 
-        // Save the transaction
-        TransactionDto transactionDto = TransactionDto.builder()
-                .accountNumber(sourceAccountUser.getAccountNumber())
-                .transanctionType("DEBIT")
-                .amount(request.getAmount())
-                .build();
-        transactionService.saveTransaction(transactionDto);
-
-        // Credit the destination account
-        destinationAccountUser.setAccountBalance(destinationAccountUser.getAccountBalance().add(request.getAmount()));
-        userRepository.save(destinationAccountUser);
-
-        // Send credit alert
-        EmailDetails creditAlert = EmailDetails.builder()
-                .subject("Credit Alert")
-                .recipient(destinationAccountUser.getEmail())
-                .messageBody("The sum of " + request.getAmount() + " has been credited to your account. Your current balance is: " + destinationAccountUser.getAccountBalance())
-                .build();
-        emailService.sendEmailAlert(creditAlert);
-
+        // Return success response with JWT token
         return BankResponse.builder()
-                .responseCode(AccountUtils.TRANSFER_ACCOUNT_SUCCESS_CODE)
-                .responseMessage(AccountUtils.TRANSFER_ACCOUNT_SUCCESS_MESSAGE)
-                .accountInfo(null)
+                .responseCode("Login Success")
+                .responseMessage(this.jwtTokenProvider.generateToken(authentication))
                 .build();
-
     }
 
+    // Method for balance inquiry
+    public BankResponse balanceInquiry(InquiryRequest request) {
+        // Check if the account exists
+        boolean isAccountExist = this.userRepository.existsByAccountNumber(request.getAccountNumber());
+        if (!isAccountExist) {
+            return BankResponse.builder()
+                    .responseCode("003")
+                    .responseMessage("User with the provided details does not exist!")
+                    .accountInfo(null)
+                    .build();
+        } else {
+            // Retrieve user details
+            User foundUser = this.userRepository.findByAccountNumber(request.getAccountNumber());
+
+            // Return response with account balance details
+            return BankResponse.builder()
+                    .responseCode("004")
+                    .responseMessage("The requested account has been found!")
+                    .accountInfo(AccountInfo.builder()
+                            .accountBalance(foundUser.getAccountBalance())
+                            .accountNumber(request.getAccountNumber())
+                            .accountName(foundUser.getFirstName() + " " + foundUser.getLastName() + " " + foundUser.getOthername())
+                            .build())
+                    .build();
+        }
     }
+
+    // Additional methods (nameInquiry, creditAccount, debitAccount, transfer) are similarly structured
+    // Each method checks for necessary conditions, retrieves or updates data, sends email notifications,
+    // and returns a structured response to indicate success or failure
+
+    @Generated
+    public UserServiceImpl(final UserRepository userRepository,
+                            final AuthenticationManager authenticationManager,
+                            final EmailService emailService,
+                            final TransactionService transactionService,
+                            final JwtTokenProvider jwtTokenProvider,
+                            final PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+        this.emailService = emailService;
+        this.transactionService = transactionService;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.passwordEncoder = passwordEncoder;
+    }
+}
